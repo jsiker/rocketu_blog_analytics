d1e1a76cdcaaca353b75fd22fdab4864d9f71269
Jon Siker <jon.siker@gmail.com>
2014-10-23 15:20:08 -0700

    mixpanel

--- rocketu_blog_analytics/settings.py
+++ rocketu_blog_analytics/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 MEDIA_URL = "/media/"
